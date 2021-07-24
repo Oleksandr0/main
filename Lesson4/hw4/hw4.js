da33a82eb224b27e3d1a9fb7d7a1921b52197602
Oleksandr0 <iermolchev80@gmail.com>
2021-07-25 02:29:33 +0300

    Completed cw4

--- Lesson4/hw4/hw4.js
+++ Lesson4/hw4/hw4.js
@@ -98,43 +98,43 @@
 // - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
 // створити пустий масив, наповнити його 10 об'єктами new User(....)
 // Взяти масив цей  User[] та: Відфільтрувати , залишивши тільки об'єкти з парними id (filter) та Відсортувати його по id. по зростанню (sort)
-
-class User{
-    constructor(id,name,surname,email,phone) {
-        this.id=id;
-        this.name=name;
-        this.surname=surname;
-        this.email=email;
-        this.phone=phone;
-
-    }
-}
-const dima= new User(1,'Dima','Ivanov','dim@gmail.com','0673874747');
-const rima= new User(2,'Rima','Ivanova','rima@gmail.com','0678462233');
-const vova= new User(3,'Vova','Petrov','vova@gmail.com','0673870564');
-const vika= new User(4,'Vika','Petrova','vika@gmail.com','0679889393');
-const ira= new User(5,'Ira','Sidorova','ira@gmail.com','0671237878');
-const vasya= new User(6,'Vasya','Vasin','vasya@gmail.com','0677874848');
-const kolya= new User(7,'Kolya','Usenko','kolya@gmail.com','0672873478');
-const sima= new User(8,'Sima','Simenko','sima@gmail.com','0675578493');
-const dasha= new User(9,'Dasha','Pupkina','dasha@gmail.com','0683645457');
-const petya= new User(10,'Petya','Pupkin','petya@gmail.com','0673667587');
-
-//console.log(dima)
-
-let user=[];
-
-user.push(dima,rima,vova,vika,ira,vasya,kolya,sima,dasha,petya)
-
-console.log(user);
-let filter=user.filter(function (users){
-    return users.id %2==0;
-
-});
-console.log(filter);
-
-user.sort(function (a, b){
-    return b.id-a.id;
-});
-console.log(user);
+//
+// class User{
+//     constructor(id,name,surname,email,phone) {
+//         this.id=id;
+//         this.name=name;
+//         this.surname=surname;
+//         this.email=email;
+//         this.phone=phone;
+//
+//     }
+// }
+// const dima= new User(1,'Dima','Ivanov','dim@gmail.com','0673874747');
+// const rima= new User(2,'Rima','Ivanova','rima@gmail.com','0678462233');
+// const vova= new User(3,'Vova','Petrov','vova@gmail.com','0673870564');
+// const vika= new User(4,'Vika','Petrova','vika@gmail.com','0679889393');
+// const ira= new User(5,'Ira','Sidorova','ira@gmail.com','0671237878');
+// const vasya= new User(6,'Vasya','Vasin','vasya@gmail.com','0677874848');
+// const kolya= new User(7,'Kolya','Usenko','kolya@gmail.com','0672873478');
+// const sima= new User(8,'Sima','Simenko','sima@gmail.com','0675578493');
+// const dasha= new User(9,'Dasha','Pupkina','dasha@gmail.com','0683645457');
+// const petya= new User(10,'Petya','Pupkin','petya@gmail.com','0673667587');
+//
+// //console.log(dima)
+//
+// let user=[];
+//
+// user.push(dima,rima,vova,vika,ira,vasya,kolya,sima,dasha,petya)
+//
+// console.log(user);
+// let filter=user.filter(function (users){
+//     return users.id %2==0;
+//
+// });
+// console.log(filter);
+//
+// user.sort(function (a, b){
+//     return b.id-a.id;
+// });
+// console.log(user);
 
